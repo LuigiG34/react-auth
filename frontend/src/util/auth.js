c0068252db615cef327c0a148a3d2e30239ecf53
Luigi Gandemer <luigigandemer6@gmail.com>
2026-03-18 17:45:43 +0100

    Adding route protection

--- frontend/src/util/auth.js
+++ frontend/src/util/auth.js
@@ -1,8 +1,19 @@
+import {redirect} from "react-router-dom";
+
 export function getAuthToken() {
-    const token = localStorage.getItem('token');
-    return token;
+    return localStorage.getItem('token');
 }
 
 export function tokenLoader() {
     return getAuthToken();
+}
+
+export function checkAuthLoader() {
+    const token = getAuthToken();
+
+    if(!token) {
+        return redirect('/auth?mode=login');
+    }
+
+    return null;
 }
